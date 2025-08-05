7e0978f945141a4a97c15e7d736f11d5bdefc157
Tenzing Sherpa <pasangsherpa689@gmail.com>
2025-08-05 15:41:50 +0800

    variables

--- Main.java
+++ Main.java
@@ -1,5 +1,28 @@
+
 public class Main {
     public static void main(String[] args) {
+        
+        
+        String name = "Ming";
+        int x;
+        x = 22;
+
+        System.out.println(x);
+
+        System.out.println(name);
+
         System.out.println("Hello World");
+        System.out.println(33);
+        System.out.println(3 * 4);
+
     }
+
 }
+
+// this is a comment in java
+
+/*
+ * this is a multi line comments
+ * this is a comment
+ * 
+ */
