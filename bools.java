1644f53c403d63401a2834b0569f2191b1ed380a
Mingma Tenzing Sherpa <mingmatenzingsherpa@Mac.lan>
2025-08-06 15:54:26 +0800

    java bools

---
+++ bools.java
@@ -0,0 +1,11 @@
+public class bools {
+    public static void main(String[] args) {
+        
+        boolean isMingmaReal = true;
+        if (isMingmaReal == false) {
+            System.out.println("Mings true bruh");
+        }
+            System.out.println(10==4);
+
+    }
+}
---
+++ math.java
@@ -0,0 +1,12 @@
+public class math {
+    public static void main(String[] args) {
+        int x, y, z;
+        x = 2;
+        y = 5;
+        z = 9;
+        System.out.println(Math.max(x, y));
+       
+        
+
+    }
+}
