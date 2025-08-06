b5d6c51d7f4570040edd6286fcf18061a3d07d05
Mingma Tenzing Sherpa <mingmatenzingsherpa@Mac.lan>
2025-08-06 23:11:46 +0800

    ternary operator in if else

--- bools.java
+++ bools.java
@@ -1,11 +1,16 @@
 public class bools {
     public static void main(String[] args) {
         
-        boolean isMingmaReal = true;
-        if (isMingmaReal == false) {
-            System.out.println("Mings true bruh");
-        }
-            System.out.println(10==4);
+        // boolean isMingmaReal = true;
+        // if (isMingmaReal == false) {
+        //     System.out.println("Mings true bruh");
+        // }
+        //     System.out.println(10==4);
 
+
+        int x = 10;
+        String result = x < 12 ? "x is less than 12" : "x is greater";
+   
+  System.out.println(result); 
     }
 }
