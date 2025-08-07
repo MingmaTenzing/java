8175d053ddebab8d83c5b9e9d44e2b54c655f5af
Mingma Tenzing Sherpa <mingmatenzingsherpa@Mac.lan>
2025-08-08 00:12:28 +0800

    loops

---
+++ loops.java
@@ -0,0 +1,30 @@
+public class loops {
+    public static void main(String[] args) {
+
+        // int i = 1; 
+        // while(i<10) {
+
+        //     System.out.println("hello");
+        //     i++;
+        // }
+
+        // for (int i = 0; i < 10; i++) {
+        //     System.out.println(i);
+        // }
+
+        // for (int i = 5; i > 0; i--) {
+        //     System.out.println(i);
+        // }
+
+        for (int i = 0; i < 2; i++) {
+            System.out.println("i'm outer loop" + " " + i);
+            for (int j = 0; j <= 3; j++) {
+            System.out.println("i'm inner loop " + " " + j + " " + "with inner loop" + " " + i);
+        }
+        
+        }
+    }
+
+
+
+}
