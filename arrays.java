c22cdf9edeed027068f7daa0d8daedb10c91983f
Mingma Tenzing Sherpa <mingmatenzingsherpa@Mac.lan>
2025-08-08 00:50:11 +0800

    arrays.java

---
+++ arrays.java
@@ -0,0 +1,13 @@
+public class arrays {
+    public static void main(String[] args) {
+        String[] cars = { "Toyota", "BMW", "Honda", "Kia" };
+        
+        System.out.println(cars.length);
+        System.out.println(cars[0]);
+
+        for (int i = 0; i < cars.length; i++) {
+    System.out.println(cars[i]);
+ }
+
+    }
+}
