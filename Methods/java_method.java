e0e479ba4cb1d7edcc9b8869d1ff6167901c2483
Mingma Tenzing Sherpa <mingmatenzingsherpa@Mac.lan>
2025-08-09 17:12:23 +0800

    java method params

--- Methods/java_method.java
+++ Methods/java_method.java
@@ -1,13 +1,28 @@
 public class java_method {
 
-    public static void callme() {
-        System.out.println("i'm did y ou call me ");
+    public static String callme() {
+        String template = "Did you call me";
+        return template; 
     }
+
+    public static void call_your_name(String provided_name) {
+
+        System.out.println(callme() + provided_name);
+    
+    
+    
+    }
+
 
     public static void main(String[] args) {
         callme();
         callme();
         callme();
+
+        call_your_name("mingma");
     }
-    
+
 }
+
+
+
