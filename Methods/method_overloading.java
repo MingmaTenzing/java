8a1a54269ed2d9c40aafbe8987f09065a18653c4
Mingma Tenzing Sherpa <pasangsherpa689@gmail.com>
2025-08-10 00:11:31 +0800

    method overloading

---
+++ Methods/method_overloading.java
@@ -0,0 +1,22 @@
+public class method_overloading {
+
+    static int addtwonums(int x , int y) {
+
+        return x + y;
+
+    }
+
+    public static double addtwonums(double x, double y) {
+        return x + y;
+
+    }
+
+    public static void main(String[] args) {
+        int intsum = addtwonums(4, 8);
+        double doubleSum = addtwonums(4.3, 1.1);
+
+        System.out.println(intsum);
+        System.out.println(doubleSum);
+    }
+    
+}
