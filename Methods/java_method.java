5cafd69af30a50775ff0ce847fee485cd246c764
Mingma Tenzing Sherpa <mingmatenzingsherpa@Mac.lan>
2025-08-09 16:57:56 +0800

    java methods

---
+++ Methods/java_method.java
@@ -0,0 +1,13 @@
+public class java_method {
+
+    public static void callme() {
+        System.out.println("i'm did y ou call me ");
+    }
+
+    public static void main(String[] args) {
+        callme();
+        callme();
+        callme();
+    }
+    
+}
