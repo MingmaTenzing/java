8074adeca624502071a9b35bfa0db99ade65f242
Tenzing Sherpa <pasangsherpa689@gmail.com>
2025-08-11 16:02:58 +0800

    oop

---
+++ Classes/oop.java
@@ -0,0 +1,14 @@
+public class oop {
+
+   
+        String name = "Toyota";
+        
+    
+    public static void main(String[] args) {
+
+        oop myCar = new oop();
+        oop mysecondCar = new oop();
+        System.out.println(myCar.name + " " +  mysecondCar.name);
+
+    }
+}
