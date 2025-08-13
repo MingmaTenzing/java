fc2b3cb52144d48584040762c409d35799f3d955
Mingma Tenzing Sherpa <pasangsherpa689@gmail.com>
2025-08-13 15:07:22 +0800

    class methods complete

---
+++ Classes/first.java
@@ -0,0 +1,10 @@
+public class first {
+    public void fullthrottle() {
+        System.out.println(" Full throttle is on");
+
+    }
+
+    public void speed(int maxspeed) {
+        System.out.println("The max speed currently is " + " " + maxspeed);
+    }
+}
---
+++ Classes/second.java
@@ -0,0 +1,8 @@
+public class second {
+
+    public static void main(String[] args) {
+        first myCar = new first();
+        myCar.fullthrottle();
+        myCar.speed(100);
+    }
+}
