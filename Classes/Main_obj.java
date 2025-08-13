750d8c955c97c4e07e063ec104f47f73f92d8440
Mingma Tenzing Sherpa <pasangsherpa689@gmail.com>
2025-08-13 13:43:18 +0800

    publicvsstatic

--- Classes/Main_obj.java
+++ Classes/Main_obj.java
@@ -2,6 +2,18 @@ public class Main_obj {
    
     int x = 5;
     int y = 3;
+
+     static class Car {
+        String name ;
+        String brand;
+        String trim ;
+    }
+
+
+
+    static int showvalue(Main_obj obj) {
+        return obj.x;
+    }
     
     public static void main(String[] args) {
 
@@ -9,5 +21,19 @@ public class Main_obj {
         mynewObj.x = 3;
         mynewObj.y = 4;
         System.out.println(mynewObj.x + " " + mynewObj.y);
+   
+        System.out.println(Main_obj.showvalue(mynewObj));
+
+        Car myCar = new Car();
+        myCar.brand = "Toyota";
+        myCar.name = "Aurion";
+        myCar.trim = "Touring";
+
+        System.out.println(myCar.brand);
+        System.out.println(myCar.name);
+        System.out.println(myCar.trim);
+        
+
+
     }
 }
