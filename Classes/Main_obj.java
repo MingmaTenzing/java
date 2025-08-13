72c3b8d0dc18b2ce60d94a70ee56e8d8b4ba4b6e
Mingma Tenzing Sherpa <pasangsherpa689@gmail.com>
2025-08-13 13:28:28 +0800

    attributes

---
+++ Classes/Main_obj.java
@@ -0,0 +1,13 @@
+public class Main_obj {
+   
+    int x = 5;
+    int y = 3;
+    
+    public static void main(String[] args) {
+
+        Main_obj mynewObj = new Main_obj();
+        mynewObj.x = 3;
+        mynewObj.y = 4;
+        System.out.println(mynewObj.x + " " + mynewObj.y);
+    }
+}
