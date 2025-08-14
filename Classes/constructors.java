d0065788ed2e0aea6f09037a2fcc8bf661e482d3
Mingma Tenzing Sherpa <pasangsherpa689@gmail.com>
2025-08-14 15:58:04 +0800

    constructors

---
+++ Classes/constructors.java
@@ -0,0 +1,21 @@
+public class constructors {
+
+    int x;
+
+    public constructors() {
+
+        x = 5;
+
+        System.out.println(x);
+
+    }
+    
+    public static void main(String[] args) {
+        constructors myint = new constructors();
+
+     
+    }
+    
+
+    
+}
