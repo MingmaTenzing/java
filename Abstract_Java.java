ba3b9827d72ef4739031681ce7e18a31f266cf04
Mingma Tenzing Sherpa <pasangsherpa689@gmail.com>
2025-08-27 11:33:51 +0800

    understanding abstraction in java

--- Abstract_Java.java
+++ Abstract_Java.java
@@ -1,30 +1,44 @@
-abstract class Animal {
-    public abstract void animalSound();
+// abstract class Absctract_Animal {
+//     abstract  void makesSound();
+// }
 
-    public void sleep() {
 
-        System.out.println("animal is sleeping");
+// class Cat extends Absctract_Animal {
+//     @Override
+//     public void makesSound() {
+//         System.out.println("cat memou meou");
+//     }
+// }
+
+// public class Abstract_Java {
+//     public static void main(String[] args) {
+//         Cat mycat = new Cat();
+//         mycat.makesSound();
+//     }
+// }
+
+
+abstract class Payment {
+    abstract void PaymentMethod();
+
+    public void process_credit() {
+        System.out.println("processing payment");
     }
-
 }
 
- class Pig extends Animal {
-
-    public void animalSound() {
-
-        System.out.println("The pgi is we we");
-    }
+class DebitPayment extends Payment {
+    @Override
+    public void PaymentMethod() {
+        System.out.println("this is debit payment");
+     }
 }
-public   class Abstract_Java {
+
+public class Abstract_Java {
+    public static void main(String[] args) {
+        DebitPayment newdebit = new DebitPayment();
+        newdebit.PaymentMethod();
+        newdebit.process_credit();
+    }
 
 
-
-
-        public static void main(String[] args) {
-            Pig myPig = new Pig();
-            myPig.animalSound();
-            myPig.sleep();
-        }
-        
-  
 }
--- firstobj.java
+++
@@ -1,4 +0,0 @@
-public class firstobj {
-    
-    int x = 5;
-}
