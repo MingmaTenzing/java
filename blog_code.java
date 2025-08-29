3906cea08693ba348e19bb2b3d7596a45977fa22
Tenzing Sherpa <pasangsherpa689@gmail.com>
2025-08-29 11:55:46 +0800

    taking input

---
+++ blog_code.java
@@ -0,0 +1,21 @@
+public class blog_code {
+
+String name = "Mingma";
+
+
+
+public static void main(String[] args) {
+
+    int age = 24;
+    
+    blog_code myobj = new blog_code(); 
+    
+    System.out.println(myobj.name);
+    System.out.println(age);
+
+}
+
+
+
+}
+
--- test.java
+++ test.java
@@ -1,12 +1,14 @@
-public class test {
-    final String Name = "ming";
+public class test 
+ {
+     String Name = "ming";
 
     public static void main(String[] args) {
         
         test mytest = new test();
+        int age = 10;
         System.out.println(mytest.Name);
 
-        
+        System.out.println(age);
 
 
     }
